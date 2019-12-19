725e29d894fd3e0a210ca5b38369ab521961100d
sujiehao <sujiehao@itcast.cn>
2019-12-19 17:17:02 +0800

    分类页样式_数据请求_tab切换

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,25 +1,44 @@
 Page({
   data:{
-    cateArr: wx.getStorageSync('cateArr') || []
+    cateArr: wx.getStorageSync('cateArr') || [],
+    cateRight: wx.getStorageSync('cateRight') || [],
+    activeIndex: 0
+  },
+  // 点击切换索引的事件处理函数
+  changeIndex(event){
+    // console.log(event);
+    const { index } = event.currentTarget.dataset;
+
+    // 小程序更新数据用 this.setData({ })
+    this.setData({ 
+      activeIndex: index,
+      cateRight: this.data.cateArr[index].children
+    })
+
+
   },
 
+  // 加载生命周期函数
   onLoad(){
     if(this.data.cateArr.length === 0){
       wx.request({
         url:"https://api.zbztb.cn/api/public/v1/categories",
         success:res=>{
           const cateArr =  res.data.message;
+          const cateRight = cateArr[0].children;
           // ❌ 网页端的本地存储写法
           // localStorage.setItem('cateArr', JSON.stringify(cateArr));
           // ✅ 小程序的本地存储写法
           wx.setStorageSync('cateArr', cateArr);
+          wx.setStorageSync('cateRight', cateRight);
           // 异步版本 - 了解
           // wx.setStorage({
           //   key: "cateArr",
           //   data: cateArr
           // });
           this.setData({
-            cateArr
+            cateArr,
+            cateRight
           });
         }
       })
