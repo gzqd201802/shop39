4b71ebf6d0f37e0114f97892460c4cf6cb9e01ef
sujiehao <sujiehao@itcast.cn>
2019-12-22 15:26:49 +0800

    商品详情数据渲染

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,25 @@
 // pages/goods_detail/index.js
+const app = getApp();
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    // 商品详情
+    goods_detail:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  async onLoad(options){
+    // goods/detail
+    const res = await app.myAxios({
+      url:'goods/detail',
+      data: options
+    });
+    // console.log(res);
+    this.setData({
+      goods_detail : res
+    });
 
   }
 })
