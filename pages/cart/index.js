744ecf0ebeff5d9f5726163c7065d6c61a5f6537
sujiehao <sujiehao@itcast.cn>
2019-12-23 17:07:46 +0800

    购物车列表的事件

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -20,6 +20,61 @@ Page({
       cartList: wx.getStorageSync('cartList') || [],
     });
 
+    // 重新计算总价格，全选状态，并更新 cartList 页面数据 和 cartList 本地存储数据
+    this.computedCartData();
+  },
+
+  // 计数器加减号事件
+  changeCount(event) {
+    // 解构事件的参数
+    const { index, number } = event.currentTarget.dataset;
+    // 解构购物车数组
+    const { cartList } = this.data;
+
+    // 如果点击的是减号，并且当前数量为 1 
+    if (number === -1 && cartList[index].goods_count === 1) {
+      // 模态提示框，有两个按钮
+      wx.showModal({
+        title: '是否删除商品',
+        showCancel: true,
+        cancelText: '取消',
+        cancelColor: '#000000',
+        confirmText: '确定',
+        confirmColor: '#3CC51F',
+        // PS: 模态提示框，不管点击确定还是取消都触发 success
+        success: (result) => {
+          // confirm 为 true，表示点击了确定
+          if(result.confirm){
+            // console.log('点了确定');
+            cartList.splice(index,1);
+            // 重新计算总价格，全选状态，并更新 cartList 页面数据 和 cartList 本地存储数据
+            this.computedCartData();
+          }
+          // cancel 为 true，表示点击了取消
+          else if(result.cancel){
+            console.log('点了取消');
+          }
+        }
+      });
+    }else{
+      // 当前商品累加
+      cartList[index].goods_count += number;
+      // 重新计算总价格，全选状态，并更新 cartList 页面数据 和 cartList 本地存储数据
+      this.computedCartData();
+    }
+
+  },
+  // 列表项的选择按钮点击
+  changeCheck(event) {
+    // 解构事件的参数
+    const { index } = event.currentTarget.dataset;
+    // 解构购物车数组
+    const { cartList } = this.data;
+
+    // 通过索引值找到数据，把自己取反
+    cartList[index].goods_selected = !cartList[index].goods_selected;
+
+    // 重新计算总价格，全选状态，并更新 cartList 页面数据 和 cartList 本地存储数据
     this.computedCartData();
   },
   // 全选按钮点击事件
@@ -34,22 +89,19 @@ Page({
       v.goods_selected = checkAll
     });
 
-    // console.log(cartList);
-    this.setData({
-      checkAll,
-      cartList
-    });
-
-    // 重新计算总价格
+    // 重新计算总价格，全选状态，并更新 cartList 页面数据 和 cartList 本地存储数据
     this.computedCartData();
 
   },
   // 封装一个计算总价格的函数
   computedCartData(){
 
+    // 解构 data 中的购物车数据
     const { cartList } = this.data;
 
+    // 总价格
     let totalPrice = 0;
+    // 选中数量
     let totalCount = 0;
 
     // 计算总价格
@@ -63,11 +115,14 @@ Page({
       }
     });
 
+    // 计算总价格，全选状态，并更新 cartList 页面数据 和 cartList 本地存储数据
     // 更新数据分两部分：页面数据和本地存储数据
     // 更新页面数据
     this.setData({
       totalPrice,
       totalCount,
+      // 购物车数据视图更新
+      cartList,
       // 全选状态，购物条数 和 选中的数量比较，相对返回 true 全选，不相等反之
       checkAll: cartList.length === totalCount
     });
