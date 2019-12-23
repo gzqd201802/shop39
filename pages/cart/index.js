a5a2fcf88b607698aaf3b733fe8f49a91fc97a99
sujiehao <sujiehao@itcast.cn>
2019-12-23 15:48:37 +0800

    购物车收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,62 +5,84 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    address: wx.getStorageSync('address') || {}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 测试代码 - 获取授权情况
+  getSetting(){
+    // PS:用户如果在授权的时候点击了拒绝授权，相当于把功能授权情况关闭了
+    wx.getSetting({
+      success: (result)=>{
+        // result 中是用户授权的情况
+        console.log(result);
+      },
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 测试代码 - 打开设置界面
+  openSetting(){
+    wx.openSetting({
+      success: (result)=>{
+        console.log(result);
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 获取收货地址终极写法，调用前需要获取用户是否授权
+  getAddressHandle(){
+    // 1.0 获取用户授权情况
+    wx.getSetting({
+      success: (result)=>{
+        // 1.1 判断用户是否地址没有授权，注意需要用三个等于号
+        if(result.authSetting["scope.address"] === false){
+          // 2.0 打开设置界面
+          wx.openSetting({
+            success: (result)=>{
+              // 3.0 调用收货地址接口
+              this.getAddress();
+            }
+          });
+        }
+        // 1.2 如果已经授权
+        else{
+          // 3.0 调用收货地址接口
+          this.getAddress();
+        }
+      },
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 获取用户地址信息
+  getAddress(){
+    // 小程序提供的获取收货地址的API
+    wx.chooseAddress({
+      success: (result)=>{
+        // console.log(result);
+        // 从成功的回调函数中提取关键信息
+        const {cityName,countyName,detailInfo,nationalCode,postalCode,provinceName,telNumber,userName} = result;
+        // 构建一个新的对象
+        const address = {
+          cityName,
+          countyName,
+          detailInfo,
+          nationalCode,
+          postalCode,
+          provinceName,
+          telNumber,
+          userName,
+          // 额外拼接了一个新的字符串
+          addressDetail:`${provinceName}${cityName}${countyName}${detailInfo}`
+        }
+        // 把地址信息保存到本地存储
+        wx.setStorageSync('address', address);
+        // 更新页面数据
+        this.setData({
+          address 
+        });
+      },
+      // 如果用户拒绝了授权，只会触发调用失败的回调函数
+      fail: (err)=>{
+        wx.showToast({
+          title: '你取消了地址选择',
+          icon: 'none'
+        });
+      }
+    });
   }
 })
